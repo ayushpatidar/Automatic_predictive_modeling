9d3653a8b2b3f67f734ffae63ed4479bcf8a76d7
ayushpatidar <ayush.patidar4@gmail.com>
2018-10-08 12:46:32 +0530

    modified

--- mysqlclient.py
+++ mysqlclient.py
@@ -1,38 +1,54 @@
 """DATABASE CONNECTIVITYY FILE..."""
-
 import MySQLdb
+import os
+import warnings
+import sys
 
-try:
+warnings.filterwarnings('ignore')
+
+
+def connection():
+    
+
     db = MySQLdb.connect("localhost", "root", "ayushpatidar@04", "TIME_SERIES")
     print("DB conencted")
-except Exception as e:
-    print("DB not connected,{}".format(e))
-
-cur = db.cursor()
-
-
-try:
-    sql = "DROP TABLE IF EXISTS RESULTS"
-    cur.execute(sql)
-
-except Exception as e:
-    print("error while dropping table,{}".format(e))
-
-try:
-
-    sql = """CREATE TABLE RESULTS(TRAINING_ID INT NOT NULL AUTO_INCREMENT,
-                                  MODEL_NAME VARCHAR(100) NULL,
-                                  MODEL_ACCURACY FLOAT NULL,
-                                  TRAINING_ERROR VARCHAR(550) NULL,
-                                  PRIMARY KEY(TRAINING_ID)
-                                  )"""
-
-    cur.execute(sql)
-    print("table created")
-
-    return cur
 
 
 
-except Exception as e:
-    print("error while creating table,{}".format(e))
+    cur = db.cursor()
+
+    try:
+        f = 0
+        sql = """SHOW TABLES"""
+        cur.execute(sql)
+        rs = cur.fetchall()
+        rs = [item[0] for item in rs]
+        if "RESULTS" in rs:
+            f = 1
+    except Exception as e:
+        print("error while fetching table,{}".format(e))
+        exit()
+
+    try:
+        if f==0:
+            sql = """CREATE TABLE RESULTS(TRAINING_ID INT NOT NULL AUTO_INCREMENT,
+                                          MODEL_NAME VARCHAR(100) NULL,
+                                          MODEL_ACCURACY FLOAT NULL,
+                                          TRAINING_ERROR VARCHAR(550) NULL,
+                                          PRIMARY KEY(TRAINING_ID)
+                                          )"""
+
+            cur.execute(sql)
+            print("table created")
+            #db.close()
+            return (db)
+        else:
+            print("TABLE IS ALREADY THERE")
+            return (db)
+
+
+
+
+    except Exception as e:
+        db.close()
+        print("error while creating table,{}".format(e))
