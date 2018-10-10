9f99eb76144fb18cbaa9a6c6bedc61b21c946eec
ayushpatidar <ayush.patidar4@gmail.com>
2018-10-10 17:48:52 +0530

    logistc module is added

--- classification_algorithms/class_algorithms.py
+++ classification_algorithms/class_algorithms.py
@@ -1,58 +1,38 @@
 import os
-import pickle
 import warnings
+import sys
+import numpy as np
 
+from sklearn.linear_model import  LogisticRegression
 from sklearn.model_selection import cross_val_score
-from sklearn.tree import DecisionTreeClassifier
 
 
-warnings.filterwarnings('ignore')
+class algorithms():
+    """all classification algorithms are
+    wriiten here"""
+    data_frame  = None
+    target = None
+
+    def set(self, data, y):
+        """This function is used to intialize the
+        field of the object of algorithms class"""
+
+        self.data_frame = data
+        self.target = y
+
+    def logistic(self):
+        """This function is for Logistic
+            Rgeression """
+        model = LogisticRegression(penalty="l1", dual=False, solver="auto")
+        results = cross_val_score(model, self.data_frame, self.target)
+        results = list(results)
+
+        score = np.mean(results)
+        return  score
 
 
-def Decision_tree(Y):
-    path = "pickle_dumps"
-
-    for fs in os.listdir(path):
-        print(fs)
-        if fs == "__init__.py":
-            continue
-        df = open(path+"/"+str(fs),"r")
-            #print(df)
-        print(df)
-        df = pickle.load(df)
-        print(type(df))
-
-        res_dc = list()
-
-        try:
-            dc = DecisionTreeClassifier(criterion = "entropy",splitter = "best",min_samples_split = 0.08,max_features = None)
-            #WRITE ALL ALGORITHMS HERE ONLY....
-            res_dc = list(cross_val_score(dc,df,Y,cv = 5))
-            print(res_dc)
-            return list(res_dc)
-
-        except Exception as e:
-            print("****error while classifying int decision tree****",e)
+    def Decision_tree(self):
 
 
 
-
-
-
-def class_algo(Y):
-
-    print("*********in classification algorithms*********")
-
-    #1.DECISION  TREE
-    try:
-        print("decision classifier is called")
-        Decision_tree(Y)
-        print("*******function returened from decision classifier******")
-
-
-    except Exception as e:
-        print("error in decision tree classifier while calling ",e)
-
-
-    #2.LOGISTIC REGRESSION
 
