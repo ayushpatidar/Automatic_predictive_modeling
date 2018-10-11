db1d33a8393ce5b7b3a69f8a832f9d25bfc3b22c
ayushpatidar <ayush.patidar4@gmail.com>
2018-10-11 17:01:50 +0530

    decision_tree added

--- classification_algorithms/class_algorithms.py
+++ classification_algorithms/class_algorithms.py
@@ -1,11 +1,14 @@
-import os
-import warnings
-import sys
+import  os
+import  sys
+import  warnings
 import numpy as np
 
 from sklearn.linear_model import  LogisticRegression
+from sklearn.tree import  DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
 
+
+warnings.filterwarnings('ignore')
 
 class algorithms():
     """all classification algorithms are
@@ -20,13 +23,16 @@ class algorithms():
         self.data_frame = data
         self.target = y
 
+
     def logistic(self):
         """This function is for Logistic
             Rgeression """
         score = None
+        model = None
+
         try:
             model = LogisticRegression(penalty="l1", dual=False, solver="auto")
-            results = cross_val_score(model, self.data_frame, self.target)
+            results = cross_val_score(model, self.data_frame, self.target, cv=5)
             results = list(results)
 
             score = np.mean(results)
@@ -34,8 +40,27 @@ class algorithms():
         except Exception as e:
             print("error in logistic regression,{}".format(e))
 
-        return  score
+        return (score, model)
 
 
 
+    def Decision_tree(self):
 
+        model = None
+        score = None
+
+        try:
+            mdoel = DecisionTreeClassifier(criterion="entropy", splitter="best",
+                                   min_samples_split=df.shape[0]*0.1,
+                                   max_features=None,)
+            results = cross_val_score(model, self.data_frame, self.target, cv=5)
+            results = list(results)
+
+            score = np.mean(results)
+
+
+        except Exception as  e:
+            print("error in decision_tree classsifying,{}".format(e))
+
+
+        results score
