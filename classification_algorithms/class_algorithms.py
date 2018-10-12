877e735e3b8d5d130ec9ab01a06d7f4b81ee76ba
ayushpatidar <ayush.patidar4@gmail.com>
2018-10-12 16:26:24 +0530

    Random Forest added

--- classification_algorithms/class_algorithms.py
+++ classification_algorithms/class_algorithms.py
@@ -2,9 +2,9 @@ import  os
 import  sys
 import  warnings
 import numpy as np
-
 from sklearn.linear_model import  LogisticRegression
 from sklearn.tree import  DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 
 
@@ -24,7 +24,7 @@ class algorithms():
         self.target = y
 
 
-    def logistic(self):
+    def Logistic(self):
         """This function is for Logistic
             Rgeression """
         score = None
@@ -43,14 +43,13 @@ class algorithms():
         return (score, model)
 
 
-
     def Decision_tree(self):
 
         model = None
         score = None
 
         try:
-            mdoel = DecisionTreeClassifier(criterion="entropy", splitter="best",
+            model = DecisionTreeClassifier(criterion="entropy", splitter="best",
                                    min_samples_split=df.shape[0]*0.1,
                                    max_features=None,)
             results = cross_val_score(model, self.data_frame, self.target, cv=5)
@@ -63,4 +62,28 @@ class algorithms():
             print("error in decision_tree classsifying,{}".format(e))
 
 
-        results score
+        return  (score, results)
+
+
+    def Random_forest(self):
+        """This function implements random
+        forest algorithm"""
+
+        model = None
+        score = None
+
+        try:
+            model = RandomForestClassifier(n_estimators=50,
+                                           criterion="entropy",
+                                        min_samples_split=self.data_frame.shape[0]*0.1,
+                                           bootstrap=True, oob_score=False)
+
+            results = list(cross_val_score(model, self.data_frame, self.target, cv=5))
+            score = np.mean(results)
+
+
+        except Exception as e:
+            print("error in random forest while classifying,{}".format(e))
+
+        return (score, model)
+
