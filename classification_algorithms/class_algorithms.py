8924b3a2abda6f62b80437cf820787857b27d57f
ayushpatidar <ayush.patidar4@gmail.com>
2018-10-22 12:24:37 +0530

    k- nearset- negihbors added

--- classification_algorithms/class_algorithms.py
+++ classification_algorithms/class_algorithms.py
@@ -1,4 +1,4 @@
-import  os
+import os
 import  sys
 import  warnings
 import numpy as np
@@ -6,13 +6,14 @@ from sklearn.linear_model import  LogisticRegression
 from sklearn.tree import  DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
-
+from sklearn.neighbors import KNeighborsClassifier
 
 warnings.filterwarnings('ignore')
 
 class algorithms():
     """all classification algorithms are
     wriiten here"""
+
     data_frame  = None
     target = None
 
@@ -31,7 +32,8 @@ class algorithms():
         model = None
 
         try:
-            model = LogisticRegression(penalty="l1", dual=False, solver="auto")
+            model = LogisticRegression(penalty="l1",
+                                       dual=False, solver="auto")
             results = cross_val_score(model, self.data_frame, self.target, cv=5)
             results = list(results)
 
@@ -50,8 +52,8 @@ class algorithms():
 
         try:
             model = DecisionTreeClassifier(criterion="entropy", splitter="best",
-                                   min_samples_split=df.shape[0]*0.1,
-                                   max_features=None,)
+                                           min_samples_split=df.shape[0]*0.1,
+                                           max_features=None,)
             results = cross_val_score(model, self.data_frame, self.target, cv=5)
             results = list(results)
 
@@ -75,7 +77,7 @@ class algorithms():
         try:
             model = RandomForestClassifier(n_estimators=50,
                                            criterion="entropy",
-                                        min_samples_split=self.data_frame.shape[0]*0.1,
+                                           min_samples_split=self.data_frame.shape[0]*0.1,
                                            bootstrap=True, oob_score=False,
                                            max_features="sqrt")
 
@@ -89,8 +91,31 @@ class algorithms():
         return (score, model)
 
 
-    def Naive_bayes(self):
-        """This function implements
-        naive bayes algorithm"""
+    def knn(self):
+        """This function implements knn algorithm"""
+
+        score = None
+        model = None
+
+        try:
+            mddel = KNeighborsClassifier(n_neighbors=5, weights="uniform", algorithm="auto",
+                                 metric="minkowski", n_jobs=-1)
+
+            results = list(cross_val_score(model, self.data_frame, self.target))
+
+            score = np.mean(results)
+
+
+        except Exception as e:
+            print("error while training in K-nearest neighbours,{}".format(e))
+
+        return (score, model)
+
+
+    def
+
+
+
+
 
 
